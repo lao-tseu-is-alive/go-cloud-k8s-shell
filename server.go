baa19c41e572ce1bff7179a19e04b7a6fec2d086
cgil <lao.tseu.is.alive@gmail.com>
2023-01-31 14:33:04 +0100

    corrected bug in arg to jq

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	VERSION                = "0.1.9"
+	VERSION                = "0.1.10"
 	APP                    = "go-cloud-k8s-shell"
 	defaultProtocol        = "http"
 	defaultPort            = 9999
