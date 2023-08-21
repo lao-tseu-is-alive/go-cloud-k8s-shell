5e4a7aba22aa4ab8897c047082e742fe87321c5a
cgil <lao.tseu.is.alive@gmail.com>
2023-08-21 11:40:12 +0200

    migrated base image to ubuntu:23.04

--- server.go
+++ server.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	VERSION                = "0.1.18"
+	VERSION                = "0.1.19"
 	APP                    = "go-cloud-k8s-shell"
 	defaultProtocol        = "http"
 	defaultPort            = 9999
