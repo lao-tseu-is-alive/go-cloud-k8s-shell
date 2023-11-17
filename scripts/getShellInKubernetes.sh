48ab99ee6ebbd3eb55eb3f1558c7295885bfa8ea
cgil <lao.tseu.is.alive@gmail.com>
2023-11-17 16:29:16 +0100

    added namespace used

--- scripts/getShellInKubernetes.sh
+++ scripts/getShellInKubernetes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "##  Start a single instance of my ubuntu shell image"
-kubectl run cg-shell  --image ghcr.io/lao-tseu-is-alive/go-cloud-k8s-shell:v0.1.13
+kubectl run cg-shell  --image ghcr.io/lao-tseu-is-alive/go-cloud-k8s-shell:v0.1.20 -n default
 echo "## Start an interactive shell access to my running ubuntu shell pod "
-kubectl exec --stdin --tty cg-shell -- /bin/bash
+kubectl exec --stdin --tty -n default cg-shell -- /bin/bash
