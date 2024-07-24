a225ef9e9869a420a8012017627a48e6c1cba9f8
cgil <lao.tseu.is.alive@gmail.com>
2024-07-24 14:55:46 +0200

    added embed FS

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"github.com/lao-tseu-is-alive/go-cloud-k8s-common/pkg/config"
 	"github.com/lao-tseu-is-alive/go-cloud-k8s-common/pkg/go_http"
 	"github.com/lao-tseu-is-alive/go-cloud-k8s-common/pkg/info"
 	"github.com/lao-tseu-is-alive/go-cloud-k8s-shell/pkg/version"
 	"github.com/rs/xid"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -17,10 +19,36 @@ const (
 	defaultPort       = 9999
 	defaultServerIp   = ""
 	defaultServerPath = "/"
+	defaultWebRootDir = "front/dist/"
 )
 
-func GetMyDefaultHandler(s *go_http.GoHttpServer) http.HandlerFunc {
+// content holds our static web server content.
+//
+//go:embed front/dist
+var content embed.FS
+
+func GetMyDefaultHandler(s *go_http.GoHttpServer, webRootDir string, content embed.FS) http.HandlerFunc {
 	handlerName := "GetMyDefaultHandler"
+	logger := s.GetLog()
+	logger.Printf("Initial call to %s with webRootDir:%s", handlerName, webRootDir)
+
+	// Create a subfolder filesystem to serve only the content of front/dist
+	subFS, err := fs.Sub(content, "front/dist")
+	if err != nil {
+		logger.Fatalf("Error creating sub-filesystem: %v", err)
+	}
+	// Create a file server handler for the subfolder
+	handler := http.FileServer(http.FS(subFS))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		go_http.TraceRequest(handlerName, r, logger)
+		go_http.RootPathGetCounter.Inc()
+		handler.ServeHTTP(w, r)
+	}
+}
+
+func GetInfoHandler(s *go_http.GoHttpServer) http.HandlerFunc {
+	handlerName := "GetInfoHandler"
 	logger := s.GetLog()
 	logger.Printf("Initial call to %s", handlerName)
 
@@ -44,7 +72,6 @@ func GetMyDefaultHandler(s *go_http.GoHttpServer) http.HandlerFunc {
 		data.UptimeOs = uptimeOS
 		guid := xid.New()
 		data.RequestId = guid.String()
-		go_http.RootPathGetCounter.Inc()
 		err = s.JsonResponse(w, data)
 		if err != nil {
 			logger.Printf("ERROR:  %v doing JsonResponse in %s, from IP: [%s]\n", err, handlerName, r.RemoteAddr)
@@ -64,14 +91,13 @@ func main() {
 	l.Printf("INFO: '🚀🚀 App %s version:%s  from %s'", version.APP, version.VERSION, version.REPOSITORY)
 	l.Printf("INFO: 'Starting %s version:%s HTTP server on port %s'", version.APP, version.VERSION, listenAddr)
 	server := go_http.NewGoHttpServer(listenAddr, l)
-	// curl -vv  -X POST -H 'Content-Type: application/json'  http://localhost:8080/time   ==> 405 Method Not Allowed,
-	// curl -vv  -X GET  -H 'Content-Type: application/json'  http://localhost:8080/time	==>200 OK , {"time":"2024-07-15T15:30:21+02:00"}
-	server.AddRoute("GET /hello", go_http.GetHandlerStaticPage("Hello", "Hello World!", l))
+	// curl -vv  -X GET  -H 'Content-Type: application/json'  http://localhost:9999/time	==>200 OK , {"time":"2024-07-15T15:30:21+02:00"}
 	// using new server Mux in Go 1.22 https://pkg.go.dev/net/http#ServeMux
 	mux := server.GetRouter()
+	mux.Handle("GET /info", GetInfoHandler(server))
 	mux.Handle("GET /{$}", go_http.NewMiddleware(
 		server.GetRegistry(), nil).
-		WrapHandler("GET /$", GetMyDefaultHandler(server)),
+		WrapHandler("GET /$", GetMyDefaultHandler(server, defaultWebRootDir, content)),
 	)
 	server.StartServer()
 }
