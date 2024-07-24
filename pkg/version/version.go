bde61723a10df5df5276f63ba40b39d416b60ffa
cgil <lao.tseu.is.alive@gmail.com>
2024-07-24 11:52:49 +0200

    corrected bug in path

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sShell"
 	AppSnake   = "go-cloud-k8s-shell"
-	VERSION    = "0.1.27"
+	VERSION    = "0.1.28"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-shell"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
--- scripts/getAppInfo.sh
+++ scripts/getAppInfo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "## Extracting app name and version from source"
-APP_NAME=$(grep -E 'APP\s+=' server.go| awk '{ print $3 }'  | tr -d '"')
-APP_VERSION=$(grep -E 'VERSION\s+=' server.go| awk '{ print $3 }'  | tr -d '"')
-echo "## Found APP: ${APP_NAME}, VERSION: ${APP_VERSION}  in source file server.go"
-export APP_VERSION APP_NAME
+SOURCE_CODE=pkg/version/version.go
+echo "## Extracting app name and version from code in ${SOURCE_CODE}"
+APP_NAME=$(grep -E 'APP\s+=' $SOURCE_CODE| awk '{ print $3 }'  | tr -d '"')
+APP_VERSION=$(grep -E 'VERSION\s+=' $SOURCE_CODE| awk '{ print $3 }'  | tr -d '"')
+APP_REVISION=$(grep -E 'REVISION\s+=' $SOURCE_CODE| awk '{ print $3 }'  | tr -d '"')
+APP_REPOSITORY=$(grep -E 'REPOSITORY\s+=' $SOURCE_CODE| awk '{ print $3 }'  | tr -d '"')
+APP_NAME_SNAKE=$(grep -E 'AppSnake\s+=' $SOURCE_CODE| awk '{ print $3 }'  | tr -d '"')
+echo "## Found APP: ${APP_NAME}, VERSION: ${APP_VERSION}, REVISION: ${APP_REVISION}  in source file ${SOURCE_CODE}"
+export APP_NAME APP_NAME_SNAKE APP_VERSION APP_REVISION APP_REPOSITORY
