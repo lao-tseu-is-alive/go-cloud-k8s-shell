5fcc9c8599c0bb41bf62f31c70caba57366135f2
cgil <lao.tseu.is.alive@gmail.com>
2024-08-06 09:50:06 +0200

    added debug mime types

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sShell"
 	AppSnake   = "go-cloud-k8s-shell"
-	VERSION    = "0.3.3"
+	VERSION    = "0.3.4"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-shell"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
