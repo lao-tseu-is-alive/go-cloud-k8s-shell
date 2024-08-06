d3143c029cead1d404a58b71314e3ab3414e5088
cgil <lao.tseu.is.alive@gmail.com>
2024-08-06 16:08:58 +0200

    reading ALLOWED_HOSTS

--- cmd/server/front/src/version.ts
+++ cmd/server/front/src/version.ts
@@ -1,4 +1,4 @@
 export const APP = "goCloudK8sShell";
-export const VERSION = "0.3.8";
+export const VERSION = "0.3.9";
 export const BUILD_DATE = "2024-08-06";
 export const REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-shell"
