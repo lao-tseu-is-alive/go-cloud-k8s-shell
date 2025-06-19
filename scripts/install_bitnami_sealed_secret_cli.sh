45f4062a478e4f6ce5ab9c0be1429448669865ae
cgil <lao.tseu.is.alive@gmail.com>
2025-06-19 15:54:33 +0200

    using sealed secrets with kubeseal (bitnami)

---
+++ scripts/install_bitnami_sealed_secret_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.30.0/kubeseal-0.30.0-linux-amd64.tar.gz"
+tar -xvzf kubeseal-0.30.0-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
